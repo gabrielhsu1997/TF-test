dbe70475d54070ce3b5a50fe8148c419bdc6f3c9
gabrielhsu1997 <62041749+gabrielhsu1997@users.noreply.github.com>
2021-08-24 11:57:03 +0800

    Q2 and 3

--- Q2.py
+++ Q2.py
@@ -5,4 +5,13 @@ Created on Tue Aug 24 10:27:20 2021
 @author: Gabriel
 """
 
-
+# I've tried this function but it doesn't work. 
+# NameError springs up at function call itself and I don't really know to allow for parameters that are undefined variables.
+def exists(*args):
+    for arg in args:
+        try:
+            arg
+        except NameError:
+            print(str(arg)+"is not defined.")
+        else:
+            print(str(arg)+" is defined.")
---
+++ Q3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+
+def pascaltri(n):
+    if n > 0:
+        newarr = np.zeros([n,n], dtype = int)
+        newarr[0][0] = 1
+        print(1)
+    for i in range(1, n, 1):
+        newarr[i][0] = 1
+        print(1, end = ' ')
+        for j in range(1, i+1, 1):
+            newarr[i][j] = newarr[i-1][j-1]+newarr[i-1][j]
+            print(newarr[i][j], end = ' ')
+        print('')
+
+pascaltri(10)
