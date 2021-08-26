f8070b5b8e993e5adcfcb72ecfd215942b71d6e7
gabrielhsu1997 <62041749+gabrielhsu1997@users.noreply.github.com>
2021-08-26 13:43:35 +0800

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -8,10 +8,15 @@ import yfinance as yf
 df = yf.download("AAPL IBM GOOG BP XOM COST GS", start = "2016-01-01", end = "2017-01-01")
 df = df.iloc[:,:7]
 
-# a. historical returns
+# a. via Historical Returns
 portfolio = []
 weights = [15,20,20,15,10,15,5]
 for i in range(0,len(df)):
     prices = df.iloc[i,:].tolist()
     portfolio.append((np.dot(prices,weights))/100)
-df = df.assign(Portfolio = portfolio)
+# df = df.assign(Portfolio = portfolio)
+
+returns =[0]
+for i in range(1,len(portfolio)):
+    returns.append((portfolio[i]-portfolio[i-1])/portfolio[i-1])
+VaR95 = np.quantile(returns, 0.05)
