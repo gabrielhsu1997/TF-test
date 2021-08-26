f8f0bc1433a86041e25a4c6b15550463fcd9cf4f
gabrielhsu1997 <62041749+gabrielhsu1997@users.noreply.github.com>
2021-08-26 14:37:04 +0800

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -3,20 +3,24 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
+from scipy.stats import norm
 
 
 df = yf.download("AAPL IBM GOOG BP XOM COST GS", start = "2016-01-01", end = "2017-01-01")
 df = df.iloc[:,:7]
 
 # a. via Historical Returns
-portfolio = []
-weights = [15,20,20,15,10,15,5]
-for i in range(0,len(df)):
-    prices = df.iloc[i,:].tolist()
-    portfolio.append((np.dot(prices,weights))/100)
-# df = df.assign(Portfolio = portfolio)
+weights = np.array([0.15,0.20,0.20,0.15,0.10,0.15,0.05])
+df_returns = df.pct_change()
+port_returns = []
+for i in range(1,len(df)):
+    returns = df_returns.iloc[i,:].tolist()
+    port_returns.append(np.dot(returns,weights))
 
-returns =[0]
-for i in range(1,len(portfolio)):
-    returns.append((portfolio[i]-portfolio[i-1])/portfolio[i-1])
-VaR95 = np.quantile(returns, 0.05)
+VaR95 = np.quantile(port_returns, 0.05)
+
+# b. via Parametric Method
+port_mean = np.mean(port_returns)
+cov_matrix = df_returns.cov()
+port_std = np.sqrt(np.dot(weights.T,np.dot(cov_matrix,weights)))
+newVaR95 = norm.ppf(0.05,port_mean,port_std)
